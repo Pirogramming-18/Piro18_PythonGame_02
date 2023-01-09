24142fab90e08be7f44eea7f4fe94b6d01a576e4
Junseo <monstazo911@gmail.com>
2023-01-09 16:46:50 +0900

    준서: 초성게임파일

---
+++ koreanGame.py
@@ -0,0 +1,42 @@
+import requests
+import random
+from bs4 import BeautifulSoup as bs
+
+def koreanGame(current_player, players_re):
+    print('''
+    ---------------------------------------------------------------------------
+    _   __                                   _____                         
+    | | / /                                  |  __ \                        
+    | |/ /   ___   _ __   ___   __ _  _ __   | |  \/  __ _  _ __ ___    ___ 
+    |    \  / _ \ | '__| / _ \ / _` || '_ \  | | __  / _` || '_ ` _ \  / _ 
+    | |\  \| (_) || |   |  __/| (_| || | | | | |_\ \| (_| || | | | | ||  __/
+    \_| \_/ \___/ |_|    \___| \__,_||_| |_|  \____/ \__,_||_| |_| |_| \___|
+                                                                            
+    ---------------------------------------------------------------------------
+                                                                            ''')
+    print('📖훈민정~음 훈민정~음!🎶  ')
+    korean = input('두 자리 초성을 입력해주세요 ex) ㄱㄴ : ')
+    # 스크래핑으로 초성 사이트에서 단어를 가져온다
+    word_list = []
+    for page in range(1, 4):
+        url = f"https://wordrow.kr/%EC%B4%88%EC%84%B1/{korean}/?%EC%AA%BD={page}"
+        res = requests.get(url)
+        soup = bs(res.text, "html.parser")
+        word_list += [word.get_text() for word in soup.select("body > div.content > section > div.larger > ul > li > a > b")[0:10]]
+    random.shuffle(word_list)
+    random.shuffle(players_re)
+
+    print(f'{current_player} : {word_list[0]} ! 👍')
+    players_re.remove(current_player)
+    del word_list[0]
+
+    for player in range(0,len(players_re)-1):
+        print(f'{players_re[player]} : {word_list[player]} ! 👍')
+
+    drink = players_re[len(players_re)-1]
+    print(f'{drink} : 대체 그런 단어들은 어떻게 아는거야..? 😭')
+
+    print(f'누가 마신다~? {drink}(이)가 마신다! \n{drink[0]}! 짝👏짝👏짝👏짝👏! \n{drink[1]}! 짝👏짝👏짝👏짝👏! \n워어어언 샷!')
+
+
+    return drink
